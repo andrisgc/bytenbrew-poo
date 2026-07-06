2fd53f6beeb19b09161d738b30b019e15dc8fc7a
Alexkurokawa <Alex.silva.kurokawa@gmail.com>
2026-07-06 20:29:53 -0300

    fix: Adiciona tratamento de exceção na classe MenuPedido e adiciona métodos menuDescontoPedido, menuPagamentoPedido e menuListaPedidos.

--- src/br/edu/cafeteria/view/MenuPedido.java
+++ src/br/edu/cafeteria/view/MenuPedido.java
@@ -1,71 +1,31 @@
 package br.edu.cafeteria.view;
 
+import br.edu.cafeteria.excecao.EstoqueInsuficienteException;
+import br.edu.cafeteria.excecao.PontosInsuficientesException;
+import br.edu.cafeteria.excecao.ProdutoInexistenteException;
 import br.edu.cafeteria.modelo.cliente.Cliente;
 import br.edu.cafeteria.modelo.cliente.ClienteStandard;
 import br.edu.cafeteria.modelo.cliente.ClienteVip;
+import br.edu.cafeteria.modelo.cliente.Conversao;
 import br.edu.cafeteria.modelo.pedido.ItemPedido;
 import br.edu.cafeteria.modelo.pedido.Pedido;
-import br.edu.cafeteria.servico.cadastro.CadastroCliente;
+import br.edu.cafeteria.modelo.produto.Produto;
+import br.edu.cafeteria.servico.cadastro.CadastroPedido;
 import br.edu.cafeteria.servico.cadastro.CadastroProduto;
+import br.edu.cafeteria.servico.promocao.Promocao;
 
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MenuPedido {
     Scanner scanner = new Scanner(System.in);
 
-    public Cliente menuCadastro (CadastroCliente cadastroCliente) {
-        char respostaPossuiCadastro;
-
-        System.out.println("O cliente possui cadastro? (S/N)");
-
-        while (true) {
-            respostaPossuiCadastro = scanner.next().charAt(0);
-            if (respostaPossuiCadastro == 'S') {
-                System.out.println("Insira o CPF:");
-                String cpf = scanner.next();
-                Cliente buscaCliente = cadastroCliente.buscarCliente(cpf);
-                if (buscaCliente == null) {
-                    System.out.println("Cliente não possui cadastro.");
-                    return menuDesejaCadastrar(cadastroCliente, cpf);
-                } else {
-                    return buscaCliente;
-                }
-
-            } else if (respostaPossuiCadastro == 'N') {
-                return menuDesejaCadastrar(cadastroCliente, null);
-            } else {
-                System.out.println("Opção inválida. Por favor, digite novamente. (S/N)");
-            }
-        }
-    }
-
-    public Cliente menuDesejaCadastrar (CadastroCliente cadastroCliente, String cpf) {
-        char respostaDesejaCadastrar;
-        System.out.println("Deseja realizar o cadastro? (S/N)");
-
-        while (true) {
-            respostaDesejaCadastrar = scanner.next().charAt(0);
-            if (respostaDesejaCadastrar == 'S') {
-                MenuCadastroCliente menuCadastroCliente = new MenuCadastroCliente();
-                if (cpf != null) {
-                    return menuCadastroCliente.menuCadastrarCliente(cadastroCliente, cpf);
-                } else {
-                    return menuCadastroCliente.menuCadastrarCliente(cadastroCliente);
-                }
-            } else if (respostaDesejaCadastrar == 'N') {
-                System.out.println("Insira o nome do cliente: ");
-                String nome = scanner.nextLine();
-                return new Cliente(nome);
-            } else {
-                System.out.println("Opção inválida. Por favor, digite novamente. (S/N)");
-            }
-        }
-    }
-
-    public void menuPedido(CadastroProduto cadastroProduto, Pedido pedido) {
+    public void menuPedido(CadastroProduto cadastroProduto, CadastroPedido cadastroPedido, Pedido pedido, ArrayList<Promocao> listaPromocoes) {
         int opcao;
 
         do {
+            MenuPrincipal.limparTela();
             System.out.println("1 - Adicionar item ao pedido");
             System.out.println("2 - Remover item do pedido");
             System.out.println("3 - Consultar cardápio");
@@ -78,13 +38,15 @@ public class MenuPedido {
                     menuPedidoAdicionar(cadastroProduto, pedido);
                     break;
                 case (2):
-                    menuPedidoRemover(cadastroProduto, pedido);
+                    menuPedidoRemover(pedido);
                     break;
                 case (3):
-                    System.out.println("EM MANUTENÇÃO");
+                    System.out.println(cadastroProduto);
+                    System.out.println("Aperte X para retornar ao menu:");
+                    MenuPrincipal.retornarMenu();
                     break;
                 case (4):
-                    menuFinalizarPedido(pedido);
+                    menuFinalizarPedido(pedido, cadastroPedido, listaPromocoes);
                     break;
                 case (5):
                     break;
@@ -93,43 +55,63 @@ public class MenuPedido {
                     break;
             }
 
-        } while (opcao != 5);
+        } while (opcao != 5 && opcao != 4);
     }
 
-    public boolean menuPedidoAdicionar(CadastroProduto cadastroProduto, Pedido pedido) {
+    public void menuPedidoAdicionar(CadastroProduto cadastroProduto, Pedido pedido) {
+        String codigoIdentificador;
         System.out.println("Insira o código do produto que deseja adicionar:");
-        String codigoIdentificador = scanner.next();
-        System.out.println("Insira a quantidade:");
-        int respostaQuantidadeItemPedido = scanner.nextInt();
+        codigoIdentificador = scanner.next();
+        Produto buscaProduto;
 
-        boolean achouItem = pedido.acharItem(codigoIdentificador);
-        if (!achouItem) {
-            ItemPedido itemPedido = new ItemPedido((cadastroProduto.buscarProduto(codigoIdentificador)), respostaQuantidadeItemPedido);
-            pedido.adicionarItem(itemPedido);
-            return true;
-        } else {
-            System.out.println("Este produto já foi adicionado ao seu pedido.");
-            System.out.println("Deseja aumentar a quantidade? (S/N)");
-            char respostaAumentarQuantidade;
-            ItemPedido itemPedido = pedido.buscarItem(codigoIdentificador);
+        try {
+            buscaProduto = cadastroProduto.buscarProduto(codigoIdentificador);
 
-            while (true) {
-                respostaAumentarQuantidade = scanner.next().charAt(0);
-                if (respostaAumentarQuantidade == 'S') {
-                    System.out.println("Digite a quantidade que deseja adicionar:");
-                    respostaQuantidadeItemPedido = scanner.nextInt();
+            int respostaQuantidadeItemPedido;
+            boolean achouItem = pedido.acharItem(codigoIdentificador);
+            if (!achouItem) {
+                System.out.println("Insira a quantidade:");
+                respostaQuantidadeItemPedido = scanner.nextInt();
+                try {
+                    ItemPedido itemPedido = new ItemPedido(buscaProduto);
+                    pedido.adicionarItem(itemPedido, respostaQuantidadeItemPedido);
+                } catch (EstoqueInsuficienteException estoqueInsuficienteException) {
+                    System.out.println(estoqueInsuficienteException.getMessage());
+                    MenuPrincipal.retornarMenu();
+                }
+            } else {
+                System.out.println("Este produto já foi adicionado ao seu pedido.");
+                System.out.println("Deseja aumentar a quantidade? (S/N)");
+                char respostaAumentarQuantidade;
+                ItemPedido itemPedido = pedido.buscarItem(codigoIdentificador);
 
-                    return itemPedido.adicionarQuantidadeItemPedido(respostaQuantidadeItemPedido);
-                } else if (respostaAumentarQuantidade == 'N') {
-                    return false;
-                } else {
-                    System.out.println("Opção inválida. Por favor, digite novamente. (S/N)");
+                while (true) {
+                    respostaAumentarQuantidade = scanner.next().charAt(0);
+                    if (respostaAumentarQuantidade == 'S') {
+                        System.out.println("Digite a quantidade que deseja adicionar:");
+                        respostaQuantidadeItemPedido = scanner.nextInt();
+
+                        try {
+                            itemPedido.adicionarQuantidadeItemPedido(respostaQuantidadeItemPedido);
+                        } catch (EstoqueInsuficienteException estoqueInsuficienteException) {
+                            System.out.println(estoqueInsuficienteException.getMessage());
+                            MenuPrincipal.retornarMenu();
+                        }
+                        break;
+                    } else if (respostaAumentarQuantidade == 'N') {
+                        break;
+                    } else {
+                        System.out.println("Opção inválida. Por favor, digite novamente. (S/N)");
+                    }
                 }
             }
+        } catch (ProdutoInexistenteException produtoInexistenteException) {
+            System.out.println(produtoInexistenteException.getMessage());
+            MenuPrincipal.retornarMenu();
         }
     }
 
-    public boolean menuPedidoRemover (CadastroProduto cadastroProduto, Pedido pedido) {
+    public void menuPedidoRemover(Pedido pedido) {
         System.out.println("Insira o código do produto que deseja remover:");
         String codigoIdentificador = scanner.next();
         System.out.println("Insira a quantidade:");
@@ -138,16 +120,17 @@ public class MenuPedido {
         boolean achouItem = pedido.acharItem(codigoIdentificador);
         if (achouItem) {
             ItemPedido itemPedido = pedido.buscarItem(codigoIdentificador);
-            return pedido.removerItem(itemPedido, respostaQuantidadeItemPedido);
+            pedido.removerItem(itemPedido, respostaQuantidadeItemPedido);
         } else {
             System.out.println("Este produto não foi adicionado ao seu pedido.");
-            return false;
         }
     }
 
-    public void menuFinalizarPedido (Pedido pedido) {
+    public void menuFinalizarPedido(Pedido pedido, CadastroPedido cadastroPedido, ArrayList<Promocao> listaPromocoes) {
+        MenuPrincipal.limparTela();
         System.out.print(pedido.toString());
         Cliente clientePedido = pedido.getClientePedido();
+        operacaoPontos operacao = operacaoPontos.adicionar;
 
         if (clientePedido instanceof ClienteVip) {
             char respostaAplicarPontos;
@@ -163,18 +146,20 @@ public class MenuPedido {
                 respostaAplicarPontos = scanner.next().charAt(0);
 
                 if (respostaAplicarPontos == 'S') {
-                    if (pontosCliente >= pontosNecessarios) {
-                        pedido.calcularDesconto(pontosNecessarios, (ClienteVip) pedido.getClientePedido());
-                        System.out.print(pedido.toString());
-                        pedido.calcularDesconto(pontosNecessarios, (ClienteVip) pedido.getClientePedido());
+                    try {
+                        pedido.calcularPontos(pontosNecessarios);
+                        System.out.print(pedido);
                         System.out.println("VALOR COM DESCONTO: " + pedido.getValorTotal());
-                        break;
-                    } else {
-                        System.out.println("O cliente não possui pontos suficientes.");
+                        operacao = operacaoPontos.remover;
+                    } catch (PontosInsuficientesException pontosInsuficientesException) {
+                        System.out.println(pontosInsuficientesException.getMessage());
+                        System.out.println("Quantidade de pontos a serem ganhos: " + pontosGanhos);
+                        System.out.println("Quantidade final de pontos do cliente: " + (pontosGanhos + pontosCliente));
                     }
+                    break;
                 } else if (respostaAplicarPontos == 'N') {
-                    System.out.println("Quantidade final de pontos: " + pontosCliente + " + " + pontosGanhos);
-                    ((ClienteVip) clientePedido).adicionarPontos(pedido.getValorTotal());
+                    System.out.println("Quantidade de pontos a serem ganhos: " + pontosGanhos);
+                    System.out.println("Quantidade final de pontos do cliente: " + (pontosGanhos + pontosCliente));
                     break;
                 } else {
                     System.out.println("Opção inválida. Por favor, digite novamente. (S/N)");
@@ -186,7 +171,95 @@ public class MenuPedido {
 
             System.out.println("Quantidade total de pontos do cliente: " + pontosCliente);
             System.out.println("Quantidade final de pontos: " + pontosCliente + " + " + pontosGanhos);
-            ((ClienteStandard) clientePedido).adicionarPontos(pedido.getValorTotal());
         }
+
+        menuDescontoPedido(pedido, listaPromocoes);
+        menuPagamentoPedido(pedido, cadastroPedido, clientePedido, operacao);
+    }
+
+    public void menuDescontoPedido(Pedido pedido, ArrayList<Promocao> listaPromocoes) {
+        char respostaRealizarDesconto;
+
+        System.out.println("Deseja aplicar desconto? (S/N)");
+        while (true) {
+            respostaRealizarDesconto = scanner.next().charAt(0);
+
+            if (respostaRealizarDesconto == 'S') {
+                int opcao;
+
+                System.out.println("Qual desconto deseja aplicar?");
+                for (int i = 0; i < listaPromocoes.size(); i++) {
+                    System.out.println((i + 1) + " - " + listaPromocoes.get(i));
+                }
+
+                while (true) {
+                    opcao = scanner.nextInt();
+                    if (opcao >= 1 && opcao <= listaPromocoes.size()) {
+                        Promocao promocao = listaPromocoes.get(opcao - 1);
+                        promocao.aplicarDesconto(pedido);
+
+                        MenuPrincipal.limparTela();
+                        System.out.println("Pedido com desconto: ");
+                        System.out.println(pedido.toStringDesconto());
+                        break;
+                    } else {
+                        System.out.println("Opção inválida. Por favor, digite novamente: ");
+                    }
+                }
+                break;
+            } else if (respostaRealizarDesconto == 'N') {
+                break;
+            } else {
+                System.out.println("Opção inválida. Por favor, digite novamente. (S/N)");
+            }
+        }
+    }
+
+    public enum operacaoPontos {
+        adicionar, remover
+    }
+
+    public void menuPagamentoPedido(Pedido pedido, CadastroPedido cadastroPedido, Cliente clientePedido, operacaoPontos operacao) {
+        char respostaRealizarPagamento;
+
+        System.out.println("Deseja realizar o pagamento? (S/N)");
+        while (true) {
+            respostaRealizarPagamento = scanner.next().charAt(0);
+
+            if (respostaRealizarPagamento == 'S') {
+                cadastroPedido.adicionarPedido(pedido);
+
+                for (ItemPedido itemPedido : pedido.getItensPedido()) {
+                    int quantidadeItemPedido = itemPedido.getQuantidadeItemPedido();
+                    Produto produtoItemPedido = itemPedido.getProdutoItemPedido();
+                    produtoItemPedido.subtrairEstoque(quantidadeItemPedido);
+                }
+
+                if (clientePedido instanceof Conversao) {
+                    if (operacao == operacaoPontos.adicionar) {
+                        ((Conversao) clientePedido).adicionarPontos(pedido.getValorTotal());
+                    } else {
+                        pedido.calcularValor();
+                        ((ClienteVip) clientePedido).removerPontos(pedido.verificarPontosNecessarios());
+                    }
+                }
+
+                System.out.println("PAGAMENTO EFETUADO COM SUCESSO");
+                MenuPrincipal.retornarMenu();
+                break;
+            } else if (respostaRealizarPagamento == 'N') {
+                System.out.println("PAGAMENTO CANCELADO");
+                MenuPrincipal.retornarMenu();
+                break;
+            } else {
+                System.out.println("Opção inválida. Por favor, digite novamente. (S/N)");
+            }
+        }
+    }
+
+    public void menuListaPedidos (CadastroPedido cadastroPedido) {
+        MenuPrincipal.limparTela();
+        System.out.println(cadastroPedido);
+        MenuPrincipal.retornarMenu();
     }
 }
