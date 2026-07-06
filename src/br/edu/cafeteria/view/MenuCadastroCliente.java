b3d911377278eb453685253459b7741fbcfb12b0
Alexkurokawa <Alex.silva.kurokawa@gmail.com>
2026-07-06 20:18:19 -0300

    fix: Adiciona tratamento de exceção na classe MenuCadastroCliente.

--- src/br/edu/cafeteria/view/MenuCadastroCliente.java
+++ src/br/edu/cafeteria/view/MenuCadastroCliente.java
@@ -1,5 +1,6 @@
 package br.edu.cafeteria.view;
 
+import br.edu.cafeteria.excecao.ClienteInexistenteException;
 import br.edu.cafeteria.modelo.cliente.Cliente;
 import br.edu.cafeteria.modelo.cliente.ClienteStandard;
 import br.edu.cafeteria.modelo.cliente.ClienteVip;
@@ -14,6 +15,7 @@ public class MenuCadastroCliente {
         int opcao;
 
         do {
+            MenuPrincipal.limparTela();
             System.out.println("1 - Cadastrar cliente");
             System.out.println("2 - Clientes cadastrados");
             System.out.println("3 - Buscar cliente");
@@ -21,13 +23,14 @@ public class MenuCadastroCliente {
             System.out.println("5 - Atualizar dados de cliente");
             System.out.println("6 - Sair");
             opcao = scanner.nextInt();
+            scanner.nextLine();
 
             switch (opcao) {
                 case (1):
                     menuCadastrarCliente(cadastroCliente);
                     break;
                 case (2):
-                    System.out.println(cadastroCliente);
+                    menuClientesCadastrados(cadastroCliente);
                     break;
                 case (3):
                     menuBuscarCliente(cadastroCliente);
@@ -42,11 +45,60 @@ public class MenuCadastroCliente {
                     break;
                 default:
                     System.out.println("Opção inválida! Digite novamente: ");
+                    break;
             }
-
         } while (opcao != 6);
+    }
 
+    public Cliente menuCadastro (CadastroCliente cadastroCliente) {
+        char respostaPossuiCadastro;
 
+        System.out.println("O cliente possui cadastro? (S/N)");
+
+        while (true) {
+            respostaPossuiCadastro = scanner.next().charAt(0);
+            if (respostaPossuiCadastro == 'S') {
+                System.out.println("Insira o CPF:");
+                String cpf = scanner.next();
+                Cliente buscaCliente;
+
+                try {
+                    buscaCliente = cadastroCliente.buscarCliente(cpf);
+                    return buscaCliente;
+                } catch (ClienteInexistenteException clienteInexistenteException) {
+                    System.out.println(clienteInexistenteException.getMessage());
+                    return menuDesejaCadastrar(cadastroCliente, cpf);
+                }
+            } else if (respostaPossuiCadastro == 'N') {
+                return menuDesejaCadastrar(cadastroCliente, null);
+            } else {
+                System.out.println("Opção inválida. Por favor, digite novamente. (S/N)");
+            }
+        }
+    }
+
+    public Cliente menuDesejaCadastrar (CadastroCliente cadastroCliente, String cpf) {
+        char respostaDesejaCadastrar;
+        System.out.println("Deseja realizar o cadastro? (S/N)");
+
+        while (true) {
+            respostaDesejaCadastrar = scanner.next().charAt(0);
+            if (respostaDesejaCadastrar == 'S') {
+                MenuCadastroCliente menuCadastroCliente = new MenuCadastroCliente();
+                if (cpf != null) {
+                    return menuCadastroCliente.menuCadastrarCliente(cadastroCliente, cpf);
+                } else {
+                    return menuCadastroCliente.menuCadastrarCliente(cadastroCliente);
+                }
+            } else if (respostaDesejaCadastrar == 'N') {
+                System.out.println("Insira o nome do cliente: ");
+                scanner.nextLine();
+                String nome = scanner.nextLine();
+                return new Cliente(nome);
+            } else {
+                System.out.println("Opção inválida. Por favor, digite novamente. (S/N)");
+            }
+        }
     }
 
     public Cliente menuCadastrarCliente(CadastroCliente cadastroCliente) {
@@ -54,7 +106,6 @@ public class MenuCadastroCliente {
         String nome;
 
         System.out.println("Insira o nome do cliente:");
-        scanner.nextLine();
         nome = scanner.nextLine();
         System.out.println("Insira o CPF do cliente:");
         cpf = scanner.nextLine();
@@ -68,7 +119,6 @@ public class MenuCadastroCliente {
         String nome;
 
         System.out.println("Insira o nome do cliente:");
-        scanner.nextLine();
         nome = scanner.nextLine();
 
         ClienteStandard clienteNovo = new ClienteStandard(nome, cpf);
@@ -80,15 +130,17 @@ public class MenuCadastroCliente {
         String cpf;
 
         System.out.println("Insira o CPF do cliente:");
-        scanner.nextLine();
         cpf = scanner.nextLine();
+        Cliente clienteBusca;
 
-        Cliente clienteBusca = cadastroCliente.buscarCliente(cpf);
-        if (clienteBusca != null) {
+        try {
+            clienteBusca = cadastroCliente.buscarCliente(cpf);
             System.out.println(clienteBusca);
-        } else {
-            System.out.println("O cliente não existe!");
+        } catch (ClienteInexistenteException clienteInexistenteException) {
+            System.out.println(clienteInexistenteException.getMessage());
         }
+
+        MenuPrincipal.retornarMenu();
     }
 
     public void menuRemoverCliente(CadastroCliente cadastroCliente) {
@@ -96,12 +148,12 @@ public class MenuCadastroCliente {
         int respostaRemoverCliente;
 
         System.out.println("Insira o CPF do cliente:");
-        scanner.nextLine();
         cpf = scanner.nextLine();
 
-        Cliente clienteRemover = cadastroCliente.buscarCliente(cpf);
+        Cliente clienteRemover;
 
-        if (clienteRemover != null) {
+        try {
+            clienteRemover = cadastroCliente.buscarCliente(cpf);
             System.out.println(clienteRemover);
             System.out.println("Deseja remover o cliente? (S/N)");
 
@@ -116,8 +168,9 @@ public class MenuCadastroCliente {
                     System.out.println("Opção inválida. Por favor, digite novamente. (S/N)");
                 }
             }
-        } else {
-            System.out.println("O cliente não existe!");
+        } catch (ClienteInexistenteException clienteInexistenteException) {
+            System.out.println(clienteInexistenteException.getMessage());
+            MenuPrincipal.retornarMenu();
         }
     }
 
@@ -125,11 +178,11 @@ public class MenuCadastroCliente {
         String cpf;
 
         System.out.println("Insira o CPF do cliente:");
-        scanner.nextLine();
         cpf = scanner.nextLine();
+        Cliente clienteCadastroAntigo;
 
-        Cliente clienteCadastroAntigo = cadastroCliente.buscarCliente(cpf);
-        if (clienteCadastroAntigo != null) {
+        try {
+            clienteCadastroAntigo = cadastroCliente.buscarCliente(cpf);
             String cpfNovo;
             String nomeNovo;
 
@@ -146,8 +199,15 @@ public class MenuCadastroCliente {
             }
 
             cadastroCliente.atualizarCliente(clienteCadastroNovo, clienteCadastroAntigo);
-        } else {
-            System.out.println("O cliente não existe!");
+        } catch (ClienteInexistenteException clienteInexistenteException) {
+            System.out.println(clienteInexistenteException.getMessage());
+            MenuPrincipal.retornarMenu();
         }
+    }
+
+    public void menuClientesCadastrados (CadastroCliente cadastroCliente) {
+        MenuPrincipal.limparTela();
+        System.out.println(cadastroCliente);
+        MenuPrincipal.retornarMenu();
     }
 }
